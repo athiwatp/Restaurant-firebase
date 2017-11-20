ade6f2c062a5669c77ea1610e3648c103e1b458e
Kullapat siribodhi <fluorishmp@gmail.com>
2017-11-20 21:16:05 +0700

    #2 - Update bill, show history bill - Add BillItem, props each list of bill

--- src/components/Bill.vue
+++ src/components/Bill.vue
@@ -1,76 +1,100 @@
 <template>
-	<v-container>
-		<v-flex xs12 sm6 offset-sm3>
-			<br>
-				<h3 class="primary--text text-xs-center">Bills</h3>
-				<v-card>
-					<v-card-title>
-						<div>
-							<p>Date: {{ latestTransaction.date }}</p>
-							<p>Time: {{ latestTransaction.time }}</p>
-							<p>Total: {{ latestTransaction.totalPrice }} Baht</p>
-							<p>Use Coupon: {{ latestTransaction.discount }}</p>
-						</div>
-					</v-card-title>
-					<v-layout row wrap>
-						<div class="ma-3" v-for="(item, key) in latestTransaction">
-							<div v-if="key !== 'date' && key !== 'time' && key !== 'discount' && key !== 'totalPrice'">
-								<v-card color="blue base" class="white--text" >
-									<v-container>
-										<v-layout xs7>
+	<v-container fluid>
+		<v-card style="overflow: hidden;">
+      <v-navigation-drawer
+        permanent
+        absolute
+        style="z-index: 1"
+      >
+        <v-list>
+          <v-list-tile ripple v-for="i in snap.slice().reverse()" @click.prevent="listTransaction(i)">
+						<v-list-tile-action>
+                <v-icon left color="green">check_circle</v-icon>
+              </v-list-tile-action>
+            <v-list-tile-title>{{ i.date }}, {{ i.time }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+			<v-container fluid>
+				<v-layout row wrap>
+					<v-flex xs12 sm6 offset-sm4 >
+							<v-layout row wrap>
+								<div v-if="clickedSnap.length !== 0">
+									<v-card offset-sm3>
+										<v-card-title>
 											<div>
-												<div class="headline">{{key}}</div>
-												<div>{{ item.description }}</div>
-												<div>Quantity: {{ item.quantity}}</div>
+												<h3 class="primary--text">Bills</h3>
+												<p>Date: {{ clickedSnap.date }}</p>
+												<p>Time: {{ clickedSnap.time }}</p>
+												<p>Total: {{ clickedSnap.totalPrice }} Baht</p>
+												<p>Use Coupon: {{ clickedSnap.discount }}</p>
 											</div>
-										</v-layout>
-										<v-flex xs5>
-											<v-card-media
-											  :src="item.imgSrc"
-											  height="100px"
-											  contain
-											></v-card-media>
-											<div>{{ item.price }}</div>
-										 </v-flex>
-									</v-container>
-								</v-card>
-							</div>
-						</div>
+										</v-card-title>
+
+										<app-bill-item :item="clickedSnap" ></app-bill-item>
+									</v-card>
+								</div>
+								<div v-else>
+									<v-card>
+										<v-card-title>
+											<div>
+												<h3 class="primary--text">Bills</h3>
+												<p>Date: {{ latestTransaction.date }}</p>
+												<p>Time: {{ latestTransaction.time }}</p>
+												<p>Total: {{ latestTransaction.totalPrice }} Baht</p>
+												<p>Use Coupon: {{ latestTransaction.discount }}</p>
+											</div>
+										</v-card-title>
+										<app-bill-item :item="latestTransaction" ></app-bill-item>
+									</v-card>
+								</div>
+							</v-layout>
+						</v-flex>
 					</v-layout>
-				</v-card>
-		</v-flex>
+				</v-container>
+	  </v-card>
 	</v-container>
 </template>
 
 
 <script>
 	import { ref, auth } from '../config/firebase'
+	import BillItem from './BillItem.vue'
 
 	export default{
 		data(){
 			return{
 				uid: '',
 				snap: [],
-				pushKey: ''
+				pushKey: '',
+				clickedSnap: []
 			}
+		},
+		components: {
+			appBillItem: BillItem
 		},
 		created(){
 			var vm = this;
 			auth.onAuthStateChanged(function(user) {
 				if (user) {
 					ref.child('Transactions').child(user.uid).on('child_added', snapshot => {
-						var snap = snapshot.val()
-						vm.snap.push(snap)
-						console.log(snap)
+						var snapValue = snapshot.val()
+						vm.snap.push(snapValue)
 					})
-
-
 				}
 			})
+
+			console.log(this.clickedSnap)
+
 		},
 		computed: {
 			latestTransaction(){
 				return this.snap[this.snap.length-1]
+			}
+		},
+		methods: {
+			listTransaction(list){
+				this.clickedSnap = list
 			}
 		}
 	}
---
+++ src/components/BillItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container fluid>
+        <div class="ma-3" v-for="(i, key) in item">
+          <div v-if="key !== 'date' && key !== 'time' && key !== 'discount' && key !== 'totalPrice'">
+            <v-card color="blue base" class="white--text" >
+              <v-container>
+                <v-layout xs7>
+                  <div>
+                    <div class="headline">{{key}}</div>
+                    <div>{{ i.description }}</div>
+                  </div>
+                </v-layout>
+                <v-flex xs5>
+                  <v-card-media
+                    :src="i.imgSrc"
+                    height="100px"
+                    contain
+                  ></v-card-media>
+                  <div>
+                    <p>Price: {{ i.price }}</p>
+                    <p>Quantity: {{ i.quantity}}</p>
+                    <p>subTotal: {{subTotal(i.quantity, i.price)}}</p>
+                  </div>
+                 </v-flex>
+              </v-container>
+            </v-card>
+          </div>
+        </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: ['item'],
+  methods: {
+    subTotal(quantity, price){
+      return quantity*price
+    }
+  }
+}
+</script>
+
+<style >
+</style>
